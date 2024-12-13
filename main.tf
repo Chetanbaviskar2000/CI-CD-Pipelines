a257e1f07a36427c21294c07021aebbaaf240956
Chetan Baviskar <113485526+Chetanbaviskar2000@users.noreply.github.com>
2024-12-13 22:56:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-1" # Change to your preferred region
   access_key = var.aws_access_key
